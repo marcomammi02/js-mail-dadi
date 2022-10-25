7805934b851ed2c972b9c91330b898867477b0a7
marcomammi02 <marcomammi2002@gmail.com>
2022-10-25 18:36:03 +0200

    Finish

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*
-
+MAIL
 creo un'array con una po di email all'interno
 chiedo all'utente la sua email
 IF l'email non è nell'array
@@ -8,12 +8,15 @@ IF l'email non è nell'array
         -aggiungo la nuova email all'array
     ELSE 
         -ricarica la pagina e ricomincia
-
 */
 
-const arrEmails = ['domenicomonaco@gmail.com', 'francescoorazini@gmail.com', 'henrikapidani@gmail.com', 'marcomammi@gmail.com' ];
-const sendBtn = document.querySelector('#sendBtn')
+
+const arrEmails = ['domenicomonaco@gmail.com', 'francescoorazini@gmail.com', 'henrikapidani@gmail.com', 'marcomammi@gmail.com', 'prova'];
+const sendBtn = document.querySelector('#sendBtn');
+const numberBtn = document.querySelector('#numberBtn')
 let registerQuestion;
+
+
 
 sendBtn.addEventListener('click', function () {
     const userEmailLogin = document.querySelector('#userEmail');
@@ -24,14 +27,48 @@ sendBtn.addEventListener('click', function () {
     if (userEmailFinder == false) {
         registerQuestion = prompt('Email errata o inesistente... Vuoi registrarti con questa email?').toLowerCase();
         if (registerQuestion == 'si') {
-            arrEmails.push(userEmail)
-            loginResult.innerHTML = ('Sei stato registrato con successo... Benvenuto ' + `<span class="marker">${userEmail}</span> ` + '! Buon divertimento...')
+            arrEmails.push(userEmail);
+            loginResult.innerHTML = ('Sei stato registrato con successo... Benvenuto ' + `<span class="marker">${userEmail}</span> ` + '! Buon divertimento...');
+            numberBtn.addEventListener('click', function (){
+                const userNumber = Math.floor(Math.random() * 7);
+                const pcNumber = Math.floor(Math.random() * 7);
+                const userResult = document.querySelector('.userResult');
+                const pcResult = document.querySelector('.pcResult');
+                const totResult = document.querySelector('.totResult');
+            
+                userResult.innerHTML = ('Il tuo numero è: ' + userNumber)
+                pcResult.innerHTML = ('Il numero del PC è: ' + pcNumber)
+                if (userNumber == pcNumber) {
+                    totResult.innerHTML = 'Pareggio!'
+                }else if (userNumber > pcNumber) {
+                    totResult.innerHTML = 'Hai vinto!'
+                }else if (userNumber < pcNumber) {
+                    totResult.innerHTML = 'Hai perso!'
+                }
+            })
         } else {
-            loginResult.innerHTML = ('Che peccato! Ricarica la pagina e riprova...')
+            loginResult.innerHTML = ('Che peccato! Ricarica la pagina e riprova...');
         }
     } else {
-        loginResult.innerHTML = ('Bentornato ' + `<span class="marker">${userEmail}</span> ` + '! Buon divertimento...')
-
+        loginResult.innerHTML = ('Bentornato ' + `<span class="marker">${userEmail}</span> ` + '! Buon divertimento...');
+        numberBtn.addEventListener('click', function (){
+            const userNumber = Math.floor(Math.random() * 7);
+            const pcNumber = Math.floor(Math.random() * 7);
+            const userResult = document.querySelector('.userResult');
+            const pcResult = document.querySelector('.pcResult');
+            const totResult = document.querySelector('.totResult');
+        
+            userResult.innerHTML = ('Il tuo numero è: ' + userNumber)
+            pcResult.innerHTML = ('Il numero del PC è: ' + pcNumber)
+            if (userNumber == pcNumber) {
+                totResult.innerHTML = 'Pareggio!'
+            }else if (userNumber > pcNumber) {
+                totResult.innerHTML = 'Hai vinto!'
+            }else if (userNumber < pcNumber) {
+                totResult.innerHTML = 'Hai perso!'
+            }
+        })
+        
     }
     
 
